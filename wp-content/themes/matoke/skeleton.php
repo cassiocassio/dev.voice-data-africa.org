affbb557717d55c44b40ecc3ca33ff517747954e
Martin Storey <martin@cassiocassio.co.uk>
2013-06-03 16:36:04 +0100

    afternoon in the photographers gallery cafe

---
+++ wp-content/themes/matoke/skeleton.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+    <script src="js/file.js"></script>  
+    <link rel="stylesheet" href="css/file.css">
+</head>
+
+
+<body>
+   <!--[if IE]>
+    <script src= »
+    "http://html5shiv.googlecode.com/svn/trunk/html5.js">
+    </script>
+  <![endif]-->
+
+<header>
+	<h1>Voice, Data: Africa</h1>
+	<img src="img/vda-logo@2x.png" alt="vda-logo@2x" width="225" height="58">
+	<h2>Photographs of a technology explosion</h2>
+	<p>A project by Martin Storey</p>
+</header>
+
+<nav>
+</nav>
+
+<section id="lead-story">
+	<img src="img/lead_story_photo.jpg" alt="lead_story_photo" width="960" height="641">
+	<h2>Beach Party</h2>
+	<h3>Selling Safaricom</h3>
+	<p>On the beach at Diani, Mombassa, during the shooting of a Safaricom video.</p>
+	<b class="keywords">Kenya</b>
+	<b class="keywords">Networks</b>
+	<b class="keywords">Advertising</b>	
+</section>
+
+<hr>
+
+<section id="featured-photostories">
+	<h1>Photostories</h1>
+	
+	<ul>
+		<li>
+			<h3>Teledensity explosion</h3>
+			<h2>A mobile for everyone</h2>
+			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+			<p>Simeon Oriko supporting the next generation of mobile developers.</p>
+		</li>
+		<li>
+			<h3>Outdoor branding</h3>
+			<h2>Red & blue & yellow </h2>
+			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+			<p>In the past, it was Soap Powder. Now, the mobile networks have painted a continent in their own unmistakable colours.</p>
+		</li>
+		<li>
+			<h3>Teledensity explosion</h3>
+			<h2>A mobile for everyone</h2>
+			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+			<p>Simeon Oriko supporting the next generation of mobile developers.</p>
+		</li>
+		<li>
+			<h3>Vodafone Ghana</h3>
+			<h2>The Main Mast</h2>
+			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+			<p>Umbelically joined to the engineering building, grows the centre of Vodafone Ghana’s network.</p>
+		</li>
+		<li>
+			<h3>iCow</h3>
+			<h2>Your calf's birthdate</h2>
+			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+			<p>Prompts farmers on vital days of cows gestation period. Helps farmers find the nearest Vet and AI providers; collects and stores farmer milk and breeding records.</p>
+		</li>
+		<li>
+			<h3>mPesa</h3>
+			<h2>Money Real Quick</h2>
+			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+			<p>A disruptive innovation that threatens incumbent businesses and is sparking new business formation and entrepreneurship. Nowhere is this mobile money phenomenon more prevalent and successful than in Kenya. Mobile money is the rare case in which a poor African country is the global market leader and an exporter of innovation.</p>
+		</li>
+		<li>
+			<h3>Nairobits</h3>
+			<h2>Young design talent growth headline</h2>
+			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+			<p>NairoBits Trust is a youth based organization that uses ICT multimedia creatively to improve the lives of less privileged children and youth from the non-formal settlement.</p>
+		</li>
+	</ul>					
+</section>
+
+<section id="explore">
+	<h1>Explore photographs</h1>
+	<div id="countries" class="narrow">
+		<h2>Countries</h2>
+			<b>Ghana</b>
+			<b>Uganda</b>
+			<b>Kenya</b>
+			<b>Liberia</b>
+			<b>Zimbabwe</b>
+	</div>
+	<div id="keywords" class="wide">
+		<h2>Countries</h2>
+			<b>Meetups</b>
+			<b>Bloggers</b>
+			<b>Hardware</b>
+			<b>KenyaMeetups</b>
+			<b>Bloggers</b>
+			<b>Hardware</b>
+			<b>KenyaStartups</b>
+			<b>Off the grid</b>
+			<b>Shopping</b>
+			<b>Call centreTeledensity</b>
+			<b>mAgriculture</b>
+			<b>Networks</b>
+			<b>Education</b>
+			<b>Hubs</b>
+<b>Teenage fashion</b>
+	</div>
+</section>
+
+<hr>
+
+<section id="Elsewhere">
+	<a href="#">
+		<h1>Elsewhere</h1>
+		<p>View all 154 links from the research scrapbook...</p>
+	</a>
+	<dl>
+		<a href="#">
+			<dt>Radio silence in Congo</dt>
+			<dd class="shortlink">voa.org/africa</dd>
+		</a>	
+			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
+		<a href="#">
+			<dt>Radio silence in Congo</dt>
+			<dd class="shortlink">voa.org/africa</dd>
+		</a>	
+			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
+		<a href="#">
+			<dt>Radio silence in Congo</dt>
+			<dd class="shortlink">voa.org/africa</dd>
+		</a>	
+			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
+		<a href="#">
+			<dt>Radio silence in Congo</dt>
+			<dd class="shortlink">voa.org/africa</dd>
+		</a>	
+			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
+		<a href="#">
+			<dt>Radio silence in Congo</dt>
+			<dd class="shortlink">voa.org/africa</dd>
+		</a>	
+			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
+		<a href="#">
+			<dt>Radio silence in Congo</dt>
+			<dd class="shortlink">voa.org/africa</dd>
+		</a>	
+			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
+		<a href="#">
+			<dt>Radio silence in Congo</dt>
+			<dd class="shortlink">voa.org/africa</dd>
+		</a>	
+			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
+	</dl>
+
+</section>
+
+
+
+
+
+
+	
+		
+			
+
+<hr>
+
+<footer>
+
+</footer>
+
+
+</body>
+
+</html>
