e75adb408481a3b780c9ff06218ea2ae2a326dfb
Martin Storey <martin@cassiocassio.co.uk>
2013-06-06 14:54:17 +0100

    in the lebanese cafe in westbourne park

--- wp-content/themes/matoke/skeleton.php
+++ wp-content/themes/matoke/skeleton.php
@@ -5,7 +5,6 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 
-<html class=“no-js”>
 
 <head>
     <meta charset="utf-8">
@@ -19,8 +18,9 @@
 
 <!--     <script src="js/vendor/modernizr-2.6.2.min.js"></script> -->
 
+<!--     <script src="js/vendor/modernizr-2.6.2.min.js"></script> -->
+	<script src="js/vendor/masonry.min.js"></script>
 </head>
-
 
 <body>
    <!--[if IE]>
@@ -29,180 +29,267 @@
     </script>
   <![endif]-->
 
-<header>
+<div id="content-frame">
+
+	<header>
 	<h1>Voice, Data: Africa</h1>
 	<h2>Photographs of a technology explosion</h2>
 </header>
-
-<section id="credit-author">A project by Martin Storey</section>
-
-<nav>
-</nav>
-
-<section id="lead-story">
-	<img src="img/lead_story_photo.jpg" class="fullwidth" alt="lead_story_photo">
-	<h2>Beach Party</h2>
-	<h3>Selling Safaricom</h3>
-	<p>On the beach at Diani, Mombassa, during the shooting of a Safaricom video.</p>
-	<ul class="keywords">
-		<li class="keywords"><a href="#">Kenya</a></li>
-		<li class="keywords"><a href="#">Networks</a></li>
-		<li class="keywords"><a href="#">Advertising</a></li>
-	</ul>	
-</section>
-
-<hr>
-
-<section id="featured-photostories">
-	<h1>Photostories</h1>
-		
-	<ul>
-		<li>
-			<h3>Teledensity explosion</h3>
-			<h2>A mobile for everyone</h2>
-			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-			<p>Simeon Oriko supporting the next generation of mobile developers.</p>
-		</li>
-		
-		
-		<li>
-			<h3>Outdoor branding</h3>
-			<h2>Red & blue & yellow </h2>
-			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-			<p>In the past, it was Soap Powder. Now, the mobile networks have painted a continent in their own unmistakable colours.</p>
-		</li>
-		<li>
-			<h3>Teledensity explosion</h3>
-			<h2>A mobile for everyone</h2>
-			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-			<p>Simeon Oriko supporting the next generation of mobile developers.</p>
-		</li>
-		<li>
-			<h3>Vodafone Ghana</h3>
-			<h2>The Main Mast</h2>
-			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-			<p>Umbelically joined to the engineering building, grows the centre of Vodafone Ghana’s network.</p>
-		</li>
-		<li>
-			<h3>iCow</h3>
-			<h2>Your calf's birthdate</h2>
-			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-			<p>Prompts farmers on vital days of cows gestation period. Helps farmers find the nearest Vet and AI providers; collects and stores farmer milk and breeding records.</p>
-		</li>
-		<li>
-			<h3>mPesa</h3>
-			<h2>Money Real Quick</h2>
-			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-			<p>A disruptive innovation that threatens incumbent businesses and is sparking new business formation and entrepreneurship. Nowhere is this mobile money phenomenon more prevalent and successful than in Kenya. Mobile money is the rare case in which a poor African country is the global market leader and an exporter of innovation.</p>
-		</li>
-		<li>
-			<h3>Nairobits</h3>
-			<h2>Young design talent growth headline</h2>
-			<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-			<p>NairoBits Trust is a youth based organization that uses ICT multimedia creatively to improve the lives of less privileged children and youth from the non-formal settlement.</p>
-		</li>
-	</ul>					
-</section>
-
-<section id="explore">
-	<h1>Explore photographs</h1>
-	<div id="countries" class="narrow">
-		<h2>Countries</h2>
-			<b>Ghana</b>
-			<b>Uganda</b>
-			<b>Kenya</b>
-			<b>Liberia</b>
-			<b>Zimbabwe</b>
-	</div>
-	<div id="keywords" class="wide">
-		<h2>Countries</h2>
-			<b>Meetups</b>
-			<b>Bloggers</b>
-			<b>Hardware</b>
-			<b>KenyaMeetups</b>
-			<b>Bloggers</b>
-			<b>Hardware</b>
-			<b>KenyaStartups</b>
-			<b>Off the grid</b>
-			<b>Shopping</b>
-			<b>Call centreTeledensity</b>
-			<b>mAgriculture</b>
-			<b>Networks</b>
-			<b>Education</b>
-			<b>Hubs</b>
-<b>Teenage fashion</b>
-	</div>
-</section>
-
-<hr>
-
-<section id="Elsewhere">
-	<a href="#">
-		<h1>Elsewhere</h1>
-		<p>View all 154 links from the research scrapbook...</p>
-	</a>
-	<dl>
-		<a href="#">
-			<dt>Radio silence in Congo</dt>
-			<dd class="shortlink">voa.org/africa</dd>
-		</a>	
-			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
-		<a href="#">
-			<dt>Radio silence in Congo</dt>
-			<dd class="shortlink">voa.org/africa</dd>
-		</a>	
-			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
-		<a href="#">
-			<dt>Radio silence in Congo</dt>
-			<dd class="shortlink">voa.org/africa</dd>
-		</a>	
-			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
-		<a href="#">
-			<dt>Radio silence in Congo</dt>
-			<dd class="shortlink">voa.org/africa</dd>
-		</a>	
-			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
-		<a href="#">
-			<dt>Radio silence in Congo</dt>
-			<dd class="shortlink">voa.org/africa</dd>
-		</a>	
-			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
-		<a href="#">
-			<dt>Radio silence in Congo</dt>
-			<dd class="shortlink">voa.org/africa</dd>
-		</a>	
-			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
-		<a href="#">
-			<dt>Radio silence in Congo</dt>
-			<dd class="shortlink">voa.org/africa</dd>
-		</a>	
-			<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
-	</dl>
-
-</section>
-
-
-
-
-
-
 	
-		
+	<section id="credit-author" itemscope itemtype="http://schema.org/Person">A project by <span itemprop="name">Martin Storey</span></section>
+	
+	<nav>
+	</nav>
+	
+	<section id="lead-story">
+		<img src="img/lead_story_photo.jpg" class="fullwidth" alt="lead_story_photo">
+		<h2>Beach Party</h2>
+		<h3>Selling Safaricom</h3>
+		<p>On the beach at Diani, Mombassa, during the shooting of a Safaricom video.</p>
+		<ul class="keywords">
+			<li class="keywords"><a href="#">Kenya</a></li>
+			<li class="keywords"><a href="#">Networks</a></li>
+			<li class="keywords"><a href="#">Advertising</a></li>
+		</ul>	
+	</section>
+	
+	<hr>
+	
+	<section id="featured-photostories">
+		<h1>Photostories</h1>
 			
+		<ul>
+			<li>
+				<h3>Teledensity explosion</h3>
+				<h2>A mobile for everyone</h2>
+				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				<p>Simeon Oriko supporting the next generation of mobile developers.</p>
+			</li>
+			
+			
+			<li>
+				<h3>Outdoor branding</h3>
+				<h2>Red & blue & yellow </h2>
+				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				<p>In the past, it was Soap Powder. Now, the mobile networks have painted a continent in their own unmistakable colours.</p>
+			</li>
+			<li>
+				<h3>Teledensity explosion</h3>
+				<h2>A mobile for everyone</h2>
+				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				<p>Simeon Oriko supporting the next generation of mobile developers.</p>
+			</li>
+			<li>
+				<h3>Vodafone Ghana</h3>
+				<h2>The Main Mast</h2>
+				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				<p>Umbelically joined to the engineering building, grows the centre of Vodafone Ghana’s network.</p>
+			</li>
+			<li>
+				<h3>iCow</h3>
+				<h2>Your calf's birthdate</h2>
+				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				<p>Prompts farmers on vital days of cows gestation period. Helps farmers find the nearest Vet and AI providers; collects and stores farmer milk and breeding records.</p>
+			</li>
+			<li>
+				<h3>mPesa</h3>
+				<h2>Money Real Quick</h2>
+				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				<p>A disruptive innovation that threatens incumbent businesses and is sparking new business formation and entrepreneurship. Nowhere is this mobile money phenomenon more prevalent and successful than in Kenya. Mobile money is the rare case in which a poor African country is the global market leader and an exporter of innovation.</p>
+			</li>
+			<li>
+				<h3>Nairobits</h3>
+				<h2>Young design talent growth headline</h2>
+				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				<p>NairoBits Trust is a youth based organization that uses ICT multimedia creatively to improve the lives of less privileged children and youth from the non-formal settlement.</p>
+			</li>
+		</ul>					
+	</section>
+	
+	<section id="explore">
+		<h1>Explore photographs</h1>
+		<div id="countries" class="keywords narrow">
+			<h4>Countries</h4>
+				<ul>
+					<li><a href="#">Ghana</a></li>
+					<li><a href="#">Uganda</a></li>
+					<li><a href="#">Kenya</a></li>
+					<li><a href="#">Liberia</a></li>
+					<li><a href="#">Zimbabwe</a></li>
+				</ul>
+		</div>
+		<div id="keywords" class="keywords wide">
+			<h4>Topics</h4>
+				<ul>
+					<li><a href="#">Meetups</a></li>
+					<li><a href="#">Bloggers</a></li>
+					<li><a href="#">Hardware</a></li>
+					<li><a href="#">KenyaMeetups</a></li>
+					<li><a href="#">Bloggers</a></li>
+					<li><a href="#">Hardware</a></li>
+					<li><a href="#">KenyaStartups</a></li>
+					<li><a href="#">Off the grid</a></li>
+					<li><a href="#">Shopping</a></li>
+					<li><a href="#">Call centre</a></li>
+					<li><a href="#">mAgriculture</a></li>
+					<li><a href="#">Networks</a></li>
+					<li><a href="#">Education</a></li>
+					<li><a href="#">Hubs</a></li>
+					<li><a href="#">Teenage fashion</a></li>
+					<li><a href="#">Teledensity</a></li>
+				</ul>
+		</div>
+	</section>
+	
+	<hr>
+	
+	<section id="elsewhere">
+		<a href="#">
+			<h1>Elsewhere as sections</h1>
+			<h4>View all 154 links from the research scrapbook...</h4>
+		</a>
+		<section class="elsewhere-item">
+			<a href="#">
+				<time datetime="1905-11-13">13-Nov-2013</time>
+				<dt>Radio silence in Congo</dt>
+				<dd class="shortlink">voa.org/africa</dd>
+			</a>	
+				<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
+		</section>	
+		
+			<section class="elsewhere-item">
+			<time datetime="1905-11-13">13-Nov-2013</time>
+			<a href="#">
+				<dt>Radio silence in Congo</dt>
+				<dd class="shortlink">voa.org/africa</dd>
+			</a>	
+				<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.
+					A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.
+				</dd>
+		</section>	
+		
+		
+			<section class="elsewhere-item">
+				<time datetime="1905-11-13">13-Nov-2013</time>
+				<a href="#">
+					<dt>Radio silence in Congo</dt>
+					<dd class="shortlink">voa.org/africa</dd>
+				</a>	
+					<dd>A lively news magazine show featuring VOA correspondent reports,  activists.</dd>
+		</section>	
+		
+		
+			<section class="elsewhere-item">
+				<time datetime="1905-11-13">13-Nov-2013</time>
+				<a href="#">
+					<dt>Radio silence in Congo</dt>
+					<dd class="shortlink">voa.org/africa</dd>
+				</a>	
+				<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activistsA lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists..</dd>
+		</section>		
+		
+			<section class="elsewhere-item" style="background:yellow;">
+				<time datetime="1905-11-13">13-Nov-2013</time>
+				<a href="#">
+					<dt>Radio silence in Congo</dt>
+					<dd class="shortlink">voa.org/africa</dd>
+				</a>	
+					<dd>A lively news magazine show featuring VOA correspondent reports,  activists.</dd>
+			</section>	
+		
+		
+			<section class="elsewhere-item">
+				<time datetime="1905-11-13">13-Nov-2013</time>
+				<a href="#">
+					<dt>Radio silence in Congo</dt>
+					<dd class="shortlink">voa.org/africa</dd>
+				</a>	
+					<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activistsA lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists..</dd>
+			</section>	
+			
+	</section> <!-- end elsewhere -->
+	
+	
+	<hr>
+	
+	<footer>
+	
+		<section id="colophon">
+			<h1>Voice, Data: Africa.</h1>
+			<p>I feel that the ways am interested in technologies as they are used by everyday people - I feel that there is a moment in history - love of the place - diversity of experience - the visual and technology.</p>
+			<p>Set in Sentinel from Typekit. Running on Wordpress at Dreamhost. Design by Cassiocassio
+			</p>
+		</section>
+		
+		<section id="contact">
+			<h1>Contact me</h1>
+			<p><a href="mailto:contact@martinstorey.com">contact@martinstorey.com</a></p>
+		
+			<div itemscope itemtype="http://schema.org/Person">
+				<p>London
+					<span itemprop="telephone"><a href="tel:+447961507427">
+						+44 7961 507427</a>
+				</span>
+				</p>
+			</div>
+			
+			<div itemscope itemtype="http://schema.org/Person">
+				<p>Kampala
+				<span itemprop="telephone">
+					<a href="tel:+256784164441">+256 7841 64441</a>
+				</span>
+				</p>
+			</div>
+			
+			<div itemscope itemtype="http://schema.org/Person">
+				<p>Accra
+				<span itemprop="telephone">
+					<a href="tel:++233547862807">+233 54 7862807</a>
+				</span>
+				</p>
+			</div>
+			
+			<div itemscope itemtype="http://schema.org/Person">
+				<p>Nairobi
+				<span itemprop="telephone">
+					<a href="tel:+234547862807">+234 54 7862807</a>
+				</span>
+				</p>
+			</div>
+		</section>
+		
+		
+		
+		<section id="image-use">
+			<h1>To use these images</h1>
+			<p>Images and text &copy;2013 Martin Storey, all rights reserved.</p>
+			<p>Ask me about terms for any use of these images, commercial or otherwise.</p>
+			<p>I’m available for commissions and assignments.</p>
+		</section>
+		
+		<section id="looking-for-more">
+			<h1>Looking for more?</h1>
+			<p>This site is a selection. You can <a href="#">browse more</a> at the full archive.</p>
+		</section>
+		
+		
+		<section id="clients">
+			<h1>Clients</h1>
+			<p><a href="#">Login here</a> to download your images.</p>
+		</section>
+	
+	
+	</footer>
 
-<hr>
+</div> <!-- end of  #content-frame -->
 
-<footer>
-
-</footer>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src='//www.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src='//www.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
 
 </body>
 
