6ada6a65b1055f712d3506b03734674f780db7c3
Martin Storey <martin@cassiocassio.co.uk>
2013-06-12 16:42:45 +0100

    at yoolakes
    have to put anchors inside dt dd

--- wp-content/themes/matoke/skeleton.php
+++ wp-content/themes/matoke/skeleton.php
@@ -13,12 +13,22 @@
     
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-    <link rel="stylesheet" href="css/style.css">
-
+    <link rel="stylesheet" href="css/style.css"> 
+   	<script src="js/vendor/jquery-1.8.3.min.js"></script>
+   	<script src="js/vendor/jquery.masonry.min.js"></script>	
+	<script src="js/vendor/jquery.dotdotdot-1.5.9.min.js"></script>
+	<script  type="text/javascript" language="javascript">
+		$(document).ready(function()   {
+			$(".ellipsis").dotdotdot();
+			$("#elsewhere-container").masonry({
+	    // options
+	    itemSelector : '.elsewhere-item',
+	    columnWidth : 217,
+	    isResizable: true
+		});
+	});	
+	</script>
 <!--     <script src="js/vendor/modernizr-2.6.2.min.js"></script> -->
-
-<!--     <script src="js/vendor/modernizr-2.6.2.min.js"></script> -->
-	<script src="js/vendor/masonry.min.js"></script>
 </head>
 
 <body>
@@ -31,12 +41,13 @@
 <div id="page-frame">
 
 	<header>
-		<p id="credit-author" itemscope itemtype="http://schema.org/Person">A project by <span itemprop="name"><a href="martinstorey.com">Martin Storey</a></p>
-
 		<div id="masthead">
 			<h1>Voice, Data: Africa</h1>
 			<h2>Photographs of the technology explosion</h2>
+			<p id="credit-author" itemscope itemtype="http://schema.org/Person">A project by <span itemprop="name"><a href="martinstorey.com">Martin Storey</a></span></p>
+
 		</div>
+
 	</header>
 	
 	<nav>
@@ -44,15 +55,19 @@
 	
 	<section id="lead-story">
 			<img src="img/lead_story_photo.jpg" alt="lead_story_photo">
-				<div id="lead-story-text">
-					<h2>Beach Party</h2>
-					<h3>Selling Safaricom</h3>
-					<p>On the beach at Diani, Mombassa, during the shooting of a Safaricom video.</p>
-					<ul class="keywords">
-						<li class="keywords"><a href="#">Kenya</a></li>
-						<li class="keywords"><a href="#">Networks</a></li>
-						<li class="keywords"><a href="#">Advertising</a></li>
-					</ul>
+				<div id="lead-story-text-group">
+					<div id="lead-story-titles">
+						<h3>Selling Safaricom</h3>
+						<h2>Beach Party</h2>
+					</div>
+					<div id="lead-story-details">
+							<p>On the beach at Diani, Mombassa, during the shooting of a Safaricom video.</p>
+							<ul class="keywords">
+								<li class="keywords"><a href="#">Kenya</a></li>
+								<li class="keywords"><a href="#">Networks</a></li>
+								<li class="keywords"><a href="#">Advertising</a></li>
+							</ul>
+					</div>
 				</div>
 	</section>
 	
@@ -60,57 +75,72 @@
 	
 
 	<section id="featured-photostories">
-		<h1>Photostories</h1>
-			
+					<h1>Photostories</h1>
 		<ul>
+
+
 			<li>
-				<h3>Teledensity explosion</h3>
-				<h2>A mobile for everyone</h2>
-				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-				<p>Simeon Oriko supporting the next generation of mobile developers.</p>
+				<a href="#">
+					<h3>Teledensity explosion</h3>
+					<h2>A mobile for everyone</h2>
+					<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				</a>
+				<p  class="ellipsis">Simeon Oriko supporting the next generation of mobile developers.</p>
 			</li>
 			
 			
 			<li>
-				<h3>Outdoor branding this one is particularly long</h3>
-				<h2>Red & blue & yellow </h2>
-				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-				<p>In the past, it was Soap Powder. Now, the mobile networks have painted a continent in their own unmistakable colours.</p>
+				<a href="#">
+					<h3>Outdoor branding this one is particularly long</h3>
+					<h2>Red & blue & yellow </h2>
+					<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				</a>	
+				<div style="word-wrap: break-word;" class="ellipsis"><p>p inside a div In the past, it was Soap Powder. Now, the mobile networks have painted a continent in their own unmistakable colours.In the past, it was Soap Powder. Now, the mobile networks have painted a continent in their own unmistakable colours.</p></div>
 			</li>
 			<li>
-				<h3>Teledensity explosion</h3>
-				<h2>A mobile for everyone</h2>
-				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-				<p>Simeon Oriko supporting the next generation of mobile developers. Simeon Oriko supporting the next generation of mobile developers. Simeon Oriko supporting the next generation of mobile developers. </p>
+				<a href="#">
+					<h3>Teledensity explosion</h3>
+					<h2>A mobile for everyone</h2>
+					<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				</a>
+				<p class="ellipsis">just a div inside the li, with class Simeon Oriko supporting the next generation of mobile developers. Simeon Oriko supporting the next generation of mobile developers. Simeon Oriko supporting the next generation of mobile developers.</p>
 			</li>
 			<li>
-				<h3>Vodafone Ghana</h3>
-				<h2>The Main Mast</h2>
-				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-				<p>Umbelically joined to the engineering building, grows the centre of Vodafone Ghana’s network.</p>
+				<a href="#">
+					<h3>Vodafone Ghana</h3>
+					<h2>The Main Mast</h2>
+					<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				</a>	
+				<p  class="ellipsis">Umbelically joined to the engineering building, grows the centre of Vodafone Ghana’s network.</p>
 			</li>
 			<li>
-				<h3>iCow</h3>
-				<h2>Your calf's birthdate this one goes one a bit</h2>
-				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-				<p>Prompts farmers on vital days of cows gestation period. Helps farmers find the nearest Vet and AI providers; collects and stores farmer milk and breeding records.</p>
+				<a href="#">
+					<h3>iCow</h3>
+					<h2>Your calf's birthdate this one goes one a bit</h2>
+					<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				</a>
+				<p  class="ellipsis">Prompts farmers on vital days of cows gestation period. Helps farmers find the nearest Vet and AI providers; collects and stores farmer milk and breeding records. Find the nearest Vet and AI providers; collects and stores farmer milk and breeding records.</p>
 			</li>
 			<li>
-				<h3>mPesa</h3>
-				<h2>Money Real Quick</h2>
-				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-				<p>A disruptive innovation that threatens incumbent businesses and is sparking new business formation and entrepreneurship. Nowhere is this mobile money phenomenon more prevalent and successful than in Kenya. Mobile money is the rare case in which a poor African country is the global market leader and an exporter of innovation.</p>
+				<a href="#">
+					<h3>mPesa</h3>
+					<h2>Money Real Quick</h2>
+					<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				</a>
+				<p class="ellipsis">A disruptive innovation that threatens incumbent businesses and is sparking new business formation and entrepreneurship. Nowhere is this mobile money phenomenon more prevalent and successful than in Kenya. Mobile money is the rare case in which a poor African country is the global market leader and an exporter of innovation.</p>
 			</li>
 			<li>
-				<h3>Nairobits</h3>
-				<h2>Young design talent growth headline</h2>
-				<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
-				<p>NairoBits Trust is a youth based organization that uses ICT multimedia creatively to improve the lives of less privileged children and youth from the non-formal settlement.</p>
+				<a href="#">
+					<h3>Nairobits</h3>
+					<h2>Young design talent growth headline</h2>
+					<img src="img/thumb-placeholder.png" alt="thumb-placeholder" width="230" height="154">
+				</a>
+				<p  class="ellipsis">NairoBits Trust is a youth based organization that uses ICT multimedia creatively to improve the lives of less privileged children and youth from the non-formal settlement.</p>
 			</li>
 		</ul>					
 	</section>
 	
-	<section id="explore">
+	<section id="explore" class="cf">
 		<h1>Explore photographs</h1>
 		<div id="countries" class="keywords narrow">
 			<h4>Countries</h4>
@@ -145,139 +175,147 @@
 		</div>
 	</section>
 		
-	<section id="elsewhere">
-		<a href="#">
-			<h1>Elsewhere as sections</h1>
-			<h4>View all 154 links from the research scrapbook...</h4>
-		</a>
-		<section class="elsewhere-item">
+		<dl>
+			<dt>This is the term</dt>
+			<dd>This is the definition></dd>
+			
+			<dt><a href="#">as anchors This is the term></a></dt>
+			<dd><a href="#">as anchors This is the definition</a></dd>
+		</dl>
+		
+		
+		
+	<section id="elsewhere" class="cf">
+		<div id="elsewhere-container"> 
 			<a href="#">
-				<time datetime="1905-11-13">13-Nov-2013</time>
-				<dt>Radio silence in Congo</dt>
-				<dd class="shortlink">voa.org/africa</dd>
-			</a>	
+				<h1>Elsewhere as sections</h1>
+				<h4>View all 154 links from the research scrapbook...</h4>
+			</a>
+			<dl>
+			<section class="elsewhere-item">
+				<dt><a href="#">Radio silence in Congo</a></dt>
+				<dd class="shortlink"><a href="#">voa.org/africa</a></dd>
 				<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.</dd>
-		</section>	
-		
-			<section class="elsewhere-item">
-			<time datetime="1905-11-13">13-Nov-2013</time>
-			<a href="#">
-				<dt>Radio silence in Congo</dt>
-				<dd class="shortlink">voa.org/africa</dd>
-			</a>	
-				<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.
-					A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.
-				</dd>
-		</section>	
-		
-		
-			<section class="elsewhere-item">
 				<time datetime="1905-11-13">13-Nov-2013</time>
-				<a href="#">
-					<dt>Radio silence in Congo</dt>
-					<dd class="shortlink">voa.org/africa</dd>
-				</a>	
-					<dd>A lively news magazine show featuring VOA correspondent reports,  activists.</dd>
-		</section>	
-		
-		
-			<section class="elsewhere-item">
-				<time datetime="1905-11-13">13-Nov-2013</time>
-				<a href="#">
-					<dt>Radio silence in Congo</dt>
-					<dd class="shortlink">voa.org/africa</dd>
-				</a>	
-				<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activistsA lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists..</dd>
-		</section>		
-		
-			<section class="elsewhere-item">
-				<time datetime="1905-11-13">13-Nov-2013</time>
-				<a href="#">
-					<dt>Radio silence in Congo</dt>
-					<dd class="shortlink">voa.org/africa</dd>
-				</a>	
-					<dd>A lively news magazine show featuring VOA correspondent reports,  activists.</dd>
-			</section>	
-		
-		
-			<section class="elsewhere-item">
-				<time datetime="1905-11-13">13-Nov-2013</time>
-				<a href="#">
-					<dt>Radio silence in Congo</dt>
-					<dd class="shortlink">voa.org/africa</dd>
-				</a>	
-					<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activistsA lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists..</dd>
+
 			</section>	
 			
+			<section class="elsewhere-item">
+					<dt><a href="#">Radio silence in Congo</a></dt>
+					<dd class="shortlink"><a href="#">voa.org/africa</a></dd>
+					
+					<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.
+						A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.
+					</dd>
+					<time datetime="1905-11-13">13-Nov-2013</time>
+			</section>	
+			
+			
+				<section class="elsewhere-item">
+					<dt><a href="#">Radio silence in Congo</a></dt>
+					<dd class="shortlink"><a href="#">voa.org/africa</a></dd>
+					
+					<dd>A lively news magazine show featuring VOA correspondent reports,  activists.</dd>
+					<time datetime="1905-11-13">13-Nov-2013</time>
+			</section>	
+			
+			
+				<section class="elsewhere-item">
+					<dt><a href="#">Radio silence in Congo</a></dt>
+					<dd class="shortlink"><a href="#">voa.org/africa</a></dd>
+				<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activistsA lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists..</dd>
+				<time datetime="1905-11-13">13-Nov-2013</time>
+			</section>		
+			
+				<section class="elsewhere-item">
+					<dt><a href="#">Radio silence in Congo</a></dt>
+					<dd class="shortlink"><a href="#">voa.org/africa</a></dd>
+					<dd>A lively news magazine show featuring VOA correspondent reports,  activists.</dd>
+				<time datetime="1905-11-13">13-Nov-2013</time>
+
+				</section>	
+			
+			
+				<section class="elsewhere-item">
+					<dt><a href="#">Radio silence in Congo</a></dt>
+					<dd class="shortlink"><a href="#">voa.org/africa</a></dd>
+					<dd>A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activistsA lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists.A lively news magazine show featuring VOA correspondent reports, interviews with African officials, opposition leaders, NGOs and human rights activists..</dd>
+				<time datetime="1905-11-13">13-Nov-2013</time>
+
+				</section>
+			</dl>
+		</div>			
 	</section> <!-- end elsewhere -->
 		
 	<footer>
-	
-		<section id="colophon">
-			<h1>Voice, Data: Africa.</h1>
-			<p>I feel that the ways am interested in technologies as they are used by everyday people - I feel that there is a moment in history - love of the place - diversity of experience - the visual and technology.</p>
-			<p>Set in Sentinel from Typekit. Running on Wordpress at Dreamhost. Design by Cassiocassio
-			</p>
-		</section>
-		
-		<section id="contact">
-			<h1>Contact me</h1>
-			<p><a href="mailto:contact@martinstorey.com">contact@martinstorey.com</a></p>
-		
-			<div itemscope itemtype="http://schema.org/Person">
-				<p>London
-					<span itemprop="telephone"><a href="tel:+447961507427">
-						+44 7961 507427</a>
-				</span>
+		<div id="footer-container" class="cf"> 
+			<section id="colophon">
+				<h1>Voice, Data: Africa.</h1>
+				<p>I feel that the ways am interested in technologies as they are used by everyday people - I feel that there is a moment in history - love of the place - diversity of experience - the visual and technology.</p>
+				<p>Set in Sentinel from Typekit. Running on Wordpress at Dreamhost. Design by Cassiocassio
 				</p>
-			</div>
+		
+				
+			</section>
 			
-			<div itemscope itemtype="http://schema.org/Person">
-				<p>Kampala
-				<span itemprop="telephone">
-					<a href="tel:+256784164441">+256 7841 64441</a>
-				</span>
-				</p>
-			</div>
+			<section id="contact">
+				<h1>Contact me</h1>
+				<p><a href="mailto:contact@martinstorey.com">contact@martinstorey.com</a></p>
 			
-			<div itemscope itemtype="http://schema.org/Person">
-				<p>Accra
-				<span itemprop="telephone">
-					<a href="tel:++233547862807">+233 54 7862807</a>
-				</span>
-				</p>
-			</div>
+				<div itemscope itemtype="http://schema.org/Person">
+					<p>London
+						<span itemprop="telephone" class="telephone-number"><a href="tel:+447961507427">
+							+44 7961 507427</a>
+					</span>
+					</p>
+				</div>
+				
+				<div itemscope itemtype="http://schema.org/Person">
+					<p>Kampala
+					<span itemprop="telephone" class="telephone-number">
+						<a href="tel:+256784164441">+256 7841 64441</a>
+					</span>
+					</p>
+				</div>
+				
+				<div itemscope itemtype="http://schema.org/Person">
+					<p>Accra
+					<span itemprop="telephone" class="telephone-number">
+						<a href="tel:++233547862807">+233 54 7862807</a>
+					</span>
+					</p>
+				</div>
+				
+				<div itemscope itemtype="http://schema.org/Person">
+					<p>Nairobi
+					<span itemprop="telephone" class="telephone-number">
+						<a href="tel:+234547862807">+234 54 7862807</a>
+					</span>
+					</p>
+				</div>
+			</section>
 			
-			<div itemscope itemtype="http://schema.org/Person">
-				<p>Nairobi
-				<span itemprop="telephone">
-					<a href="tel:+234547862807">+234 54 7862807</a>
-				</span>
-				</p>
-			</div>
-		</section>
+			
+			
+			<section id="image-use">
+				<h1>To use these images</h1>
+				<p>Images and text &copy;2013 Martin Storey, all rights reserved.</p>
+				<p>Ask me about terms for any use of these images, commercial or otherwise.</p>
+				<p>I’m available for commissions and assignments.</p>
+			</section>
+			
+			<section id="looking-for-more">
+				<h1>Looking for more?</h1>
+				<p>This site is a selection. You can <a href="#">browse more</a> at the full archive.</p>
+			</section>
+			
+			
+			<section id="clients">
+				<h1>Clients</h1>
+				<p><a href="#">Login here</a> to download your images.</p>
+			</section>
 		
-		
-		
-		<section id="image-use">
-			<h1>To use these images</h1>
-			<p>Images and text &copy;2013 Martin Storey, all rights reserved.</p>
-			<p>Ask me about terms for any use of these images, commercial or otherwise.</p>
-			<p>I’m available for commissions and assignments.</p>
-		</section>
-		
-		<section id="looking-for-more">
-			<h1>Looking for more?</h1>
-			<p>This site is a selection. You can <a href="#">browse more</a> at the full archive.</p>
-		</section>
-		
-		
-		<section id="clients">
-			<h1>Clients</h1>
-			<p><a href="#">Login here</a> to download your images.</p>
-		</section>
-	
-	
+		</div> <!-- end footer content container --> 
 	</footer>
 
 </div> <!-- end of  #content-frame -->
